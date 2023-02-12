6d5908945f0f09eafc1988cecc8fc97d170da1c8
itsferdiardiansa <ferdiardiansa@rocketmail.com>
2023-02-13 00:48:06 +0700

    fix: check

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { GraphQLSchemaHost } from '@nestjs/graphql'
 import { AppModule } from './app.module'
 import { join } from 'path'
-import { writeFileSync } from 'fs'
+import fs, { writeFileSync } from 'fs'
 import { printSchema } from 'graphql'
 
 async function bootstrap() {
@@ -13,5 +13,20 @@ async function bootstrap() {
     const { schema } = app.get(GraphQLSchemaHost);
     writeFileSync(join(process.cwd(), `/dist/schema.graphql`), printSchema(schema));
   // }
+
+   //joining path of directory 
+   const directoryPath = join(__dirname, '.')
+   //passsing directoryPath and callback function
+   fs.readdir(directoryPath, function (err, files) {
+       //handling error
+       if (err) {
+           return console.log('Unable to scan directory: ' + err)
+       } 
+       //listing all files using forEach
+       files.forEach(function (file) {
+           // Do whatever you want to do with the file
+           console.log('F: ', file) 
+       })
+   })
 }
 bootstrap()

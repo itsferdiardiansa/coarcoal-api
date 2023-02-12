cfcc4f332e36121661121df615608e35a1d0c9dc
itsferdiardiansa <ferdiardiansa@rocketmail.com>
2023-02-12 23:45:32 +0700

    fix: update vercel.json

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,5 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { GraphQLSchemaBuilderModule, GraphQLSchemaFactory } from '@nestjs/graphql'
-import { writeFileSync } from 'fs'
-import { printSchema } from 'graphql'
-import { join } from 'path'
-import { AboutMeResolver } from './about-me/about-me.resolver'
-
-// async function generateSchema() {
-//   const app = await NestFactory.create(GraphQLSchemaBuilderModule)
-//   await app.init()
-
-//   const gqlSchemaFactory = app.get(GraphQLSchemaFactory)
-//   const schema = await gqlSchemaFactory.create([
-//     AboutMeResolver
-//   ])
-
-//   console.log(printSchema(schema))
-//   // writeFileSync(join(process.cwd(), `/${serviceName}-schema.gql`), printSchema(schema))
-// }
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
